17712337b88e7f8631c2c5751dadd24dcb650a4e
rustnomicon <aquadefin@gmail.com>
2025-04-14 22:36:11 +0300

    del go

--- go.mod
+++
@@ -1,8 +0,0 @@
-module smart-bot
-
-go 1.23
-
-require (
-	github.com/joho/godotenv v1.5.1 // indirect
-	gopkg.in/telebot.v4 v4.0.0-beta.4 // indirect
-)
--- services/operations.go
+++
@@ -1,9 +0,0 @@
-package services
-
-import "fmt"
-
-// 12121 -> 10.0mb
-func BytesToMb(b uint64) string {
-	outputSizeMb := float64(b) / (1024 * 1024)
-	return fmt.Sprintf("%.2f MB (%d bytes)", outputSizeMb, b)
-}
